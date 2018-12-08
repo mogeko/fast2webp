4ae1f4265641512d24fad89c5050e39e4e938678
Mogeko <zhengjunyi@live.com>
2018-12-08 22:43:06 +0800

    update: Support for converting gif to webp

--- img2webp.py
+++ img2webp.py
@@ -8,6 +8,7 @@ import shutil
 input_path = "."# 输入路径
 output_path = None # 输出路径
 quality = "80" # 压缩程度
+enable_gif=False # 是否转换 gif 图
 
 
 # 处理传入的参数
@@ -15,12 +16,10 @@ def args(arg):
     global quality
     global input_path
     global output_path
+    global enable_gif
     i = 0
 
     while i < len(arg):
-        # 无损压缩
-        if arg[i] == "-lossless":
-            quality = arg[i]
         # 获取压缩程度
         if arg[i] == "-q":
             quality = "-q " + arg[i + 1]
@@ -30,7 +29,14 @@ def args(arg):
         # 获取输出目录
         if arg[i] == "-o":
             output_path = arg[i + 1]
+        # 无损压缩
+        if arg[i] == "-lossless":
+            quality = arg[i]
+        # gif2webp 开关
+        if (arg[i] == "-enable_gif") | (arg[i] ==  "-enable-gif"):
+            enable_gif = True
         i = i + 1
+
 
 # 判断读取的文件是否是图片
 def isPicture(file):
@@ -59,19 +65,26 @@ def cwebp(quality, input_dir, output_dir):
             # 按照输入目录的目录结构在输出的目录中创建文件夹
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
+            # 优化输入与输出文件路径
+            input_file = input_dir + "/" + file
+            output_file = output_dir + "/" + os.path.splitext(file)[0] + ".webp"
             # 判断读取的文件是否是图像文件
             if isPicture(file):
                 # cwebp
-                input = input_dir + "/" + file
-                output = output_dir + "/" + os.path.splitext(file)[0] + ".webp"
-                os.system("cwebp " + quality + " " + input + " -o " + output)
+                os.system("cwebp " + quality + " " + input_file + " -o " + output_file)
+            # 判断是否为 gif 图
+            elif (os.path.splitext(file)[1] == ".gif") & enable_gif:
+                # gif2webp 不支持无损压缩
+                if quality == "-lossless":
+                    quality = "-q 100"
+                # gif2webp
+                os.system("gif2webp " + quality + " " + input_file + " -o " + output_file)
             else:
                 # 复制多余的文件
                 if not (input_dir == output_dir) | (os.path.splitext(file)[1] == ".webp"):
-                    input = input_dir + "/" + file
-                    output = output_dir + "/" + file
-                    print("copy " + input + " to " + output)
-                    shutil.copy(input, output)
+                    output_file = output_dir + "/" + file
+                    print("copy " + input_file + " to " + output_file)
+                    shutil.copy(input_file, output_file)
 
 
 if __name__ == "__main__":
